cf512e96c603b544fff32f5e2d8c72647edd2995
Benjamin Rosenberger <rosenberger@e-conomix.at>
2019-02-21 08:43:28 +0100

    :tada: initial module structure and configs

---
+++ Api/Constants.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author      Benjamin Rosenberger <rosenberger@e-conomix.at>
+ * @package DataReporter\WebCare\Api
+ * @copyright Copyright (c) 2019 E-CONOMIX GmbH (https://www.e-conomix.at)
+ * @created 21.02.2019
+ */
+
+namespace DataReporter\WebCare\Api;
+
+
+class Constants
+{
+    const CONFIG_WEBCARE_IMPRINT = 'datareporter/webcare/enable_imprint';
+    const CONFIG_WEBCARE_PRIVACYNOTICE = 'datareporter/webcare/enable_privacy_notice';
+    const CONFIG_WEBCARE_COOKIEBANNER = 'datareporter/webcare/enable_cookie_banner';
+
+}
