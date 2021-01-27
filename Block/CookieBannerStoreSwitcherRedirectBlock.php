70a78c374a3f3544350bff70810f98e912f317cc
Benjamin Rosenberger <rosenberger@e-conomix.at>
2021-01-27 07:15:31 +0100

    :bug: fix m2 2.2.x backwards compatibility with injecting missing class

--- Block/CookieBannerStoreSwitcherRedirectBlock.php
+++ Block/CookieBannerStoreSwitcherRedirectBlock.php
@@ -8,7 +8,10 @@
 
 namespace DataReporter\WebCare\Block;
 
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\App\ObjectManager;
 use Magento\Framework\Serialize\Serializer\Json;
+use Magento\Framework\Url\EncoderInterface;
 use Magento\Framework\View\Element\Template;
 
 /**
@@ -18,30 +21,33 @@ use Magento\Framework\View\Element\Template;
 class CookieBannerStoreSwitcherRedirectBlock extends Template
 {
     protected $_template = 'DataReporter_WebCare::cookiebanner/store_switcher_redirect.phtml';
-    /**
-     * @var \Magento\Store\ViewModel\SwitcherUrlProvider
-     */
-    private $switcherUrlProvider;
+    protected $switcherUrlProvider;
     /**
      * @var Json
      */
-    private $jsonSerializer;
+    protected $jsonSerializer;
+    /**
+     * @var EncoderInterface
+     */
+    private $encoder;
 
     /**
      * CookieBannerStoreSwitcherRedirectBlock constructor.
-     * @param \Magento\Store\ViewModel\SwitcherUrlProvider $switcherUrlProvider
+     * @param EncoderInterface $encoder
+     * @param Json $jsonSerializer
      * @param Template\Context $context
      * @param array $data
      */
     public function __construct(
-        \Magento\Store\ViewModel\SwitcherUrlProvider $switcherUrlProvider,
+        EncoderInterface $encoder,
         Json $jsonSerializer,
         Template\Context $context,
         array $data = []
     ) {
         parent::__construct($context, $data);
-        $this->switcherUrlProvider = $switcherUrlProvider;
         $this->jsonSerializer = $jsonSerializer;
+        $this->encoder = $encoder;
+        $this->switcherUrlProvider = $this->getSwitcherUrlProvider();
     }
 
     public function getSwitchUrlConfiguration()
@@ -56,5 +62,38 @@ class CookieBannerStoreSwitcherRedirectBlock extends Template
             $switchConfiguration[$storeCode] = $this->switcherUrlProvider->getTargetStoreRedirectUrl($store);
         }
         return str_replace('%2C', '=', $this->jsonSerializer->serialize($switchConfiguration));
+    }
+
+    /**
+     * Runtime evaluation if SwitcherUrlProvider exists, as this class is not available vor magento2 v2.2.x installations.
+     * If injected as a constructor argument the interceptor generation would break.
+     * @return $this|mixed
+     */
+    protected function getSwitcherUrlProvider() {
+        try {
+            return ObjectManager::getInstance()->create('\Magento\Store\ViewModel\SwitcherUrlProvider');
+        } catch(\ReflectionException $e) {
+            return $this;
+        }
+    }
+
+    /**
+     * copy of function from the \Magento\Store\ViewModel\SwitcherUrlProvider class in order to be
+     * able to be backwards compatible to magento2 v2.2.x where the view model does not yet exist
+     * @param  $store
+     * @return string
+     */
+    public function getTargetStoreRedirectUrl($store)
+    {
+        return $this->_urlBuilder->getUrl(
+            'stores/store/redirect',
+            [
+                '___store' => $store->getCode(),
+                '___from_store' => $this->_storeManager->getStore()->getCode(),
+                ActionInterface::PARAM_NAME_URL_ENCODED => $this->_encoder->encode(
+                    $store->getCurrentUrl(false)
+                ),
+            ]
+        );
     }
 }
